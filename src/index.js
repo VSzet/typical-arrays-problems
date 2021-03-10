487c6bab19e2176cb8a65251228a3a146562740c
Budosinobi <61315473+Budosinobi@users.noreply.github.com>
2021-03-10 10:45:53 +0300

    feat: added code index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    let value =
+        arguments.length == 0 ? 0 : array.length == 0 ? 0 : Math.min(...array);
+    return value;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let value =
+        arguments.length == 0 ? 0 : array.length == 0 ? 0 : Math.max(...array);
+    return value;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let value =
+        arguments.length == 0
+            ? 0
+            : array.length == 0
+            ? 0
+            : array.reduce((acc, curr) => acc + curr, 0) / array.length;
+    return value;
+};
